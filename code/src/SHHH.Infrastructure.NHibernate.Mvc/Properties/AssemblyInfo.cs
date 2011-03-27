17cc0be092c9b186b3f4d2b9c59c4bf48054f550
Geoff Horsey <geoff@shhhinnovations.com>
2011-03-26 21:30:06 -0700

    removed partially trusted callers from assembly

--- code/src/SHHH.Infrastructure.NHibernate.Mvc/Properties/AssemblyInfo.cs
+++ code/src/SHHH.Infrastructure.NHibernate.Mvc/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-[assembly: AllowPartiallyTrustedCallers]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
