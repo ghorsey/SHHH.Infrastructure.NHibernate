9b5a79b0684eb7dad9706ac2655ffb36ca6b52cd
ghorsey <geoff@shhhinnovations.com>
2012-08-23 21:47:02 -0700

    added nuget files and batch files to fetch dependencies

---
+++ code/src/nuget.bat
@@ -0,0 +1,2 @@
+.nuget\nuget.exe install SHHH.Infrastructure.NHibernate\packages.config -OutputDirectory packages
+.nuget\nuget.exe install SHHH.Infrastructure.NHibernate.Mvc\packages.config -OutputDirectory packages
