c6d88ec83598742798bf2cab97915e53f002bb18
ghorsey <geoff@shhhinnovations.com>
2012-10-04 19:34:35 -0700

    2.1.1.1: Added object to collect paged results

---
+++ code/src/SHHH.Infrastructure/Collector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SHHH.Infrastructure
+{
+    public class Collector<T> where T : class
+    {
+        public IList<T> Items { get; private set; }
+        public int TotalPages { get; private set; }
+        public int CurrentPage { get; private set; }
+
+        public Collector(List<T> items, int totalPages, int currentPage)
+        {
+            Items = items;
+            TotalPages = totalPages;
+            CurrentPage = currentPage;
+        }
+    }
+}
