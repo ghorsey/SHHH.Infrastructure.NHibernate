41ae054684488ef64746cd40a7de061436f1ecca
ghorsey <geoff@shhhinnovations.com>
2012-10-11 12:27:02 -0700

    2.1.1.2: added isolation level to transactions

--- code/src/SHHH.Infrastructure.NHibernate/RepositoryBase.cs
+++ code/src/SHHH.Infrastructure.NHibernate/RepositoryBase.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using NHibernate;
+using System.Data;
 
 namespace SHHH.Infrastructure.NHibernate
 {
     public abstract class RepositoryBase
     {
-        protected void RunInTransaction(Action<ISession> f)
+        protected void RunInTransaction(Action<ISession> f, IsolationLevel isolationLevel = IsolationLevel.Unspecified)
         {
             var session = SessionSource.GetSession();
-            using (var trans = session.BeginTransaction())
+            using (var trans = session.BeginTransaction(isolationLevel))
             {
                 f(session);
+
                 if (trans.IsActive)
                     trans.Commit();
-
-                session.Flush();
             }
         }
 
