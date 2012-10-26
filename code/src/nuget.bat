577e96b3e6e54d1382f39ffbb6b862c329494eb2
ghorsey <geoff@shhhinnovations.com>
2012-10-26 13:45:57 -0700

    2.2.0.0: update to .net 4.5 and MVC 4

--- code/src/nuget.bat
+++
@@ -1,2 +0,0 @@
-.nuget\nuget.exe install SHHH.Infrastructure.NHibernate\packages.config -OutputDirectory packages
-.nuget\nuget.exe install SHHH.Infrastructure.NHibernate.Mvc\packages.config -OutputDirectory packages
