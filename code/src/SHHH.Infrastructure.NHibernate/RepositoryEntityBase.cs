85090beda5053807ff1eefae1cef897fd79e8c3b
ghorsey <geoff@shhhinnovations.com>
2012-11-27 01:26:05 -0800

    added RepositoryEntityBase

---
+++ code/src/SHHH.Infrastructure.NHibernate/RepositoryEntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using NHibernate;
+
+namespace SHHH.Infrastructure.NHibernate
+{
+    public class RepositoryEntityBase<T, TId> : RepositoryBase where T: Entity<TId>
+    {
+        public T Load(TId id, IsolationLevel isolationLevel = IsolationLevel.Unspecified, LockMode lockMode = null)
+        {
+            lockMode = lockMode ?? LockMode.None;
+            return RunInTransaction<T>(session =>
+            {
+                return session.Load<T>(id, lockMode);
+            }, isolationLevel);
+        }
+    }
+}
